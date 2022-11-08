d5c52065c01f2718a0cd03fb76aa5f499766c8ab
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2022-11-08 11:49:23 -0300

    add: litle

--- simulacao/simulacao.c
+++ simulacao/simulacao.c
@@ -3,6 +3,13 @@
 #include <math.h>
 #include <time.h>
 
+typedef struct little_
+{
+    unsigned long int no_eventos;
+    double tempo_anterior;
+    double soma_areas;
+} little;
+
 double aleatorio()
 {
     double u = rand() / ((double)RAND_MAX + 1);
@@ -22,6 +29,13 @@ double maximo(double a, double b)
     return a < b ? b : a;
 }
 
+void inicia_little(little *l)
+{
+    l->no_eventos = 0;
+    l->tempo_anterior = 0.0;
+    l->soma_areas = 0.0;
+}
+
 int main(void)
 {
     double tempo_decorrido = 0.0, tempo_simulacao;
@@ -36,6 +50,16 @@ int main(void)
 
     unsigned long int maxFila = 0;
 
+    // Little
+    little e_n;
+    little e_w_chegada;
+    little e_w_saida;
+
+    inicia_little(&e_n);
+    inicia_little(&e_w_chegada);
+    inicia_little(&e_w_saida);
+    // Little - fim
+
     printf("Informe o tempo de simulacao (seg): ");
     scanf("%lF", &tempo_simulacao);
 
@@ -46,12 +70,12 @@ int main(void)
     scanf("%lF", &tempo_medio_servico);
 
     /***************************
-	 *  Iniciando semente      *
-	 ***************************/
+     *  Iniciando semente      *
+     ***************************/
 
-	int sementeAleat;
-	sementeAleat = time(NULL);
-	srand(sementeAleat); //inicia o rand
+    int sementeAleat;
+    sementeAleat = time(NULL);
+    srand(sementeAleat); // inicia o rand
 
     // gerando o tempo de chegada da primeira requisicao
     chegada = (-1.0 / (1.0 / intervalo_medio_chegada)) * log(aleatorio());
@@ -62,7 +86,7 @@ int main(void)
 
         if (tempo_decorrido == chegada)
         {
-            printf("Chegada em %lF.\n", tempo_decorrido);
+            //printf("Chegada em %lF.\n", tempo_decorrido);
             if (!fila)
             {
                 servico = tempo_decorrido + (-1.0 / (1.0 / tempo_medio_servico)) * log(aleatorio());
@@ -71,23 +95,59 @@ int main(void)
             fila++;
             maxFila = maximo(maxFila, fila);
             chegada = tempo_decorrido + (-1.0 / (1.0 / intervalo_medio_chegada)) * log(aleatorio());
+
+            // Little
+            e_n.soma_areas += (tempo_decorrido - e_n.tempo_anterior) * e_n.no_eventos;
+            e_n.tempo_anterior = tempo_decorrido;
+            e_n.no_eventos++;
+
+            e_w_chegada.soma_areas += (tempo_decorrido - e_w_chegada.tempo_anterior) * e_w_chegada.no_eventos;
+            e_w_chegada.tempo_anterior = tempo_decorrido;
+            e_w_chegada.no_eventos++;
+            // Little - fim
         }
         else
         {
-            printf("Saída em %lF.\n", tempo_decorrido);
+            //printf("Saída em %lF.\n", tempo_decorrido);
             fila--;
             if (fila)
             {
                 servico = tempo_decorrido + (-1.0 / (1.0 / tempo_medio_servico)) * log(aleatorio());
                 soma_tempo_servico += servico - tempo_decorrido;
-
             }
+            // Little
+            e_n.soma_areas += (tempo_decorrido - e_n.tempo_anterior) * e_n.no_eventos;
+            e_n.tempo_anterior = tempo_decorrido;
+            e_n.no_eventos--;
+
+            e_w_saida.soma_areas += (tempo_decorrido - e_w_saida.tempo_anterior) * e_w_saida.no_eventos;
+            e_w_saida.tempo_anterior = tempo_decorrido;
+            e_w_saida.no_eventos++;
+            // Little - fim
         }
     }
 
-    //Qual a proporção do tempo em que o servidor ficou ocupado?
+    e_n.soma_areas += (tempo_simulacao - e_n.tempo_anterior) * e_n.no_eventos;
+    e_w_chegada.soma_areas += (tempo_simulacao - e_w_chegada.tempo_anterior) * e_w_chegada.no_eventos;
+    e_w_saida.soma_areas += (tempo_simulacao - e_w_saida.tempo_anterior) * e_w_saida.no_eventos;
+
+    double e_n_final = e_n.soma_areas / tempo_simulacao;
+    double e_w_final = (e_w_chegada.soma_areas - e_w_saida.soma_areas) / e_w_chegada.no_eventos;
+
+    printf("\nN eventos saida = %lu\n", e_w_saida.no_eventos);
+    printf("N eventos chegada = %lu\n", e_w_chegada.no_eventos);
+
+    double lambda = e_w_chegada.no_eventos / tempo_decorrido;
+
+    printf("E[N] = %lF\n", e_n_final);   
+    printf("E[W] = %lF\n", e_w_final);   
+    printf("lambda = %lF\n", lambda);   
+
+    printf("Erro de little: %.20lF\n", e_n_final - e_w_final * lambda);
+
+    // Qual a proporção do tempo em que o servidor ficou ocupado?
     printf("Ocupação: %lF\n", soma_tempo_servico / maximo(tempo_decorrido, servico));
     printf("Max fila: %ld\n", maxFila);
-    
+
     return 0;
 }
