f3a55e2df87a94ca848064bb54186e8b70c4854e
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2022-11-28 09:37:17 -0300

    add: macro for show control final result

--- trabalhos/trabalho1/simulacao_little.c
+++ trabalhos/trabalho1/simulacao_little.c
@@ -3,10 +3,12 @@
 #include <math.h>
 #include <time.h>
 
-#define OCUPACAO(x) 
-#define E_N(x) 
-#define E_W(x) 
-#define ERRO_LITTLE(x) 
+//Escolha apenas uma das macros abaixo para definir o tipo de medida que será exibida
+#define OCUPACAO(x)
+#define E_N(x)
+#define E_W(x)
+#define ERRO_LITTLE(x)
+#define VALORES_FINAIS(x) x
 
 typedef struct little_
 {
@@ -105,10 +107,10 @@ int main()
         {
             // printf("%lF,", timeControl * 100);
             double soma_areas_chegada = e_w_chegada.soma_areas +
-                (timeControl * 100.00 - e_w_chegada.tempo_anterior) * e_w_chegada.no_eventos;
+                                        (timeControl * 100.00 - e_w_chegada.tempo_anterior) * e_w_chegada.no_eventos;
 
             double soma_areas_saida = e_w_saida.soma_areas +
-                (timeControl * 100.00 - e_w_saida.tempo_anterior) * e_w_saida.no_eventos;
+                                      (timeControl * 100.00 - e_w_saida.tempo_anterior) * e_w_saida.no_eventos;
             double e_n_final = e_n.soma_areas / (timeControl * 100.00);
             double e_w_final =
                 (soma_areas_chegada - soma_areas_saida) / (double)e_w_chegada.no_eventos;
@@ -118,7 +120,7 @@ int main()
             E_W(printf(",%lF", e_w_final););
             // printf("lambda: %lF\n\n", lambda);
 
-            ERRO_LITTLE(printf(",%.20lF", e_n_final - lambda * e_w_final););
+            ERRO_LITTLE(printf(",%.20lF", fabs(e_n_final - lambda * e_w_final)););
             OCUPACAO(printf(",%lF", soma_tempo_servico / maximo(timeControl * 100.00, servico) * 100.00););
             timeControl++;
         }
@@ -182,24 +184,24 @@ int main()
         (e_w_chegada.soma_areas - e_w_saida.soma_areas) / e_w_chegada.no_eventos;
     double lambda = e_w_chegada.no_eventos / tempo_decorrido;
 
-    puts("\nResultado final:");
+    VALORES_FINAIS(
+        puts("\nResultado final:");
 
-    printf("E[N]: %lF\n", e_n_final);
-    printf("E[W]: %lF\n", e_w_final);
-    printf("lambda: %lF\n\n", lambda);
-    printf("Erro de Little: %.20lF\n\n", e_n_final - lambda * e_w_final);
-    printf("Ocupacao: %lF.\n", soma_tempo_servico / maximo(tempo_decorrido, servico));
-    printf("Max fila: %d.\n", max_fila);
+        printf("E[N]: %lF\n", e_n_final);
+        printf("E[W]: %lF\n", e_w_final);
+        printf("lambda: %lF\n\n", lambda);
+        printf("Erro de Little: %.20lF\n\n", fabs(e_n_final - lambda * e_w_final));
+        printf("Ocupacao: %lF.\n", soma_tempo_servico / maximo(tempo_decorrido, servico));
+        printf("Max fila: %d.\n", max_fila);)
 
     return 0;
 }
 
-//testando o live sh
-//e ai mermão kkkkk
-//tem chat pô
-//eu estou pelo celular kkkk
-
+// testando o live sh
+// e ai mermão kkkkk
+// tem chat pô
+// eu estou pelo celular kkkk
 
 //é daora pra caramba mesmo kkk//é daora pra caramba nesmo kkkkk// oloco kk
 // mas tem um chatzinho embutido tbm kk
-//muito bom
+// muito bom
