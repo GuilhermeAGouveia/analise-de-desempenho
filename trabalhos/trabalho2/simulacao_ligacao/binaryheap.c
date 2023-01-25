6a71853ddd309757e5efcde057cb8f212d1c14dc
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2023-01-25 11:08:34 -0300

    add: Event struct

---
+++ trabalhos/trabalho2/simulacao_ligacao/binaryheap.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binaryheap.h"
+
+int parent(int i) {
+    // Get the index of the parent
+    return (i - 1) / 2;
+}
+
+int left_child(int i) {
+    return (2*i + 1);
+}
+
+int right_child(int i) {
+    return (2*i + 2);
+}
+
+Event get_min(MinHeap* heap) {
+    // Return the root node element,
+    // since that's the minimum
+    return heap->arr[0];
+}
+
+MinHeap* init_minheap(int capacity) {
+    MinHeap* minheap = (MinHeap*) calloc (1, sizeof(MinHeap));
+    minheap->arr = (Event*) calloc (capacity, sizeof(Event));
+    minheap->capacity = capacity;
+    minheap->size = 0;
+    return minheap;
+}
+
+MinHeap* insert_minheap(MinHeap* heap, Event element) {
+    // Inserts an element to the min heap
+    // We first add it to the bottom (last level)
+    // of the tree, and keep swapping with it's parent
+    // if it is lesser than it. We keep doing that until
+    // we reach the root node. So, we will have inserted the
+    // element in it's proper position to preserve the min heap property
+    if (heap->size == heap->capacity) {
+        fprintf(stderr, "Cannot insert %d. Heap is already full!\n", element);
+        return heap;
+    }
+    // We can add it. Increase the size and add it to the end
+    heap->size++;
+    heap->arr[heap->size - 1] = element;
+
+    // Keep swapping until we reach the root
+    int curr = heap->size - 1;
+    // As long as you aren't in the root node, and while the 
+    // parent of the last element is greater than it
+    while (curr > 0 && heap->arr[parent(curr)].time > heap->arr[curr].time) {
+        // Swap
+        Event temp = heap->arr[parent(curr)];
+        heap->arr[parent(curr)] = heap->arr[curr];
+        heap->arr[curr] = temp;
+        // Update the current index of element
+        curr = parent(curr);
+    }
+    return heap; 
+}
+
+MinHeap* heapify(MinHeap* heap, int index) {
+    // Rearranges the heap as to maintain
+    // the min-heap property
+    if (heap->size <= 1)
+        return heap;
+    
+    int left = left_child(index); 
+    int right = right_child(index); 
+
+    // Variable to get the smallest element of the subtree
+    // of an element an index
+    int smallest = index; 
+    
+    // If the left child is smaller than this element, it is
+    // the smallest
+    if (left < heap->size && heap->arr[left].time < heap->arr[index].time) 
+        smallest = left; 
+    
+    // Similarly for the right, but we are updating the smallest element
+    // so that it will definitely give the least element of the subtree
+    if (right < heap->size && heap->arr[right].time < heap->arr[smallest].time) 
+        smallest = right; 
+
+    // Now if the current element is not the smallest,
+    // swap with the current element. The min heap property
+    // is now satisfied for this subtree. We now need to
+    // recursively keep doing this until we reach the root node,
+    // the point at which there will be no change!
+    if (smallest != index) 
+    { 
+        Event temp = heap->arr[index];
+        heap->arr[index] = heap->arr[smallest];
+        heap->arr[smallest] = temp;
+        heap = heapify(heap, smallest); 
+    }
+
+    return heap;
+}
+
+MinHeap* delete_minimum(MinHeap* heap) {
+    // Deletes the minimum element, at the root
+    if (!heap || heap->size == 0)
+        return heap;
+
+    int size = heap->size;
+    Event last_element = heap->arr[size-1];
+    
+    // Update root value with the last element
+    heap->arr[0] = last_element;
+
+    // Now remove the last element, by decreasing the size
+    heap->size--;
+    size--;
+
+    // We need to call heapify(), to maintain the min-heap
+    // property
+    heap = heapify(heap, 0);
+    return heap;
+}
+
+MinHeap* delete_element(MinHeap* heap, int index) {
+    // Deletes an element, indexed by index
+    // Ensure that it's lesser than the current root
+    heap->arr[index].time = get_min(heap).time - 1;
+    
+    // Now keep swapping, until we update the tree
+    int curr = index;
+    while (curr > 0 && heap->arr[parent(curr)].time > heap->arr[curr].time) {
+        Event temp = heap->arr[parent(curr)];
+        heap->arr[parent(curr)] = heap->arr[curr];
+        heap->arr[curr] = temp;
+        curr = parent(curr);
+    }
+
+    // Now simply delete the minimum element
+    heap = delete_minimum(heap);
+    return heap;
+}
+
+Event extract_minheap(MinHeap* heap) {
+    // Extracts the minimum element, and returns it
+    // We simply call delete_minimum() and return the
+    // root element
+    if (!heap || heap->size == 0)
+        return (Event) {-1, -1};
+    Event min = get_min(heap);
+    delete_minimum(heap);
+    return min;
+}
+
+void print_heap(MinHeap* heap) {
+    // Simply print the array. This is an
+    // inorder traversal of the tree
+    printf("Min Heap:\n");
+    for (int i=0; i<heap->size; i++) {
+        printf("(%d, %lf) -> ", heap->arr[i].type, heap->arr[i].time);
+    }
+    printf("\n");
+}
+
+void free_minheap(MinHeap* heap) {
+    if (!heap)
+        return;
+    free(heap->arr);
+    free(heap);
+}
---
+++ trabalhos/trabalho2/simulacao_ligacao/simulacao_ligacao.c
@@ -0,0 +1,264 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include "binaryheap.h"
+
+// Escolha apenas uma das macros abaixo para definir o tipo de medida que será exibida
+#define OCUPACAO(x) 
+#define E_N(x) 
+#define E_W(x) 
+#define ERRO_LITTLE(x) 
+#define VALORES_FINAIS(x) x
+
+typedef struct little_
+{
+    unsigned long int no_eventos;
+    double tempo_anterior;
+    double soma_areas;
+} little;
+
+double aleatorio()
+{
+    double u = rand() / ((double)RAND_MAX + 1);
+    // limitando entre (0,1]
+    u = 1.0 - u;
+
+    return (u);
+}
+
+double maximo(double num1, double num2)
+{
+    if (num1 > num2)
+    {
+        return num1;
+    }
+    return num2;
+}
+
+double minimo(double num1, double num2)
+{
+    if (num1 < num2)
+    {
+        return num1;
+    }
+    return num2;
+}
+
+double minimo_a(double *arr, int arr_size)
+{
+    int i;
+    double min = arr[0];
+    for (i = 1; i < 3; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+void inicia_little(little *l)
+{
+    l->no_eventos = 0;
+    l->tempo_anterior = 0.0;
+    l->soma_areas = 0.0;
+}
+
+int gera_pacote() {
+    int tamPacotes[3] = {1500, 40, 550};
+    int i = rand() % 10;
+    if (i < 1) {
+        i = 0;
+    } else if (i < 5) {
+        i = 1;
+    } else {
+        i = 2;
+    }
+    return tamPacotes[i];
+}
+
+void printArray(double *arr, int arr_size)
+{
+    int i;
+    for (i = 0; i < arr_size; i++)
+    {
+        printf("%lf ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    double tempo_simulacao = 36000;
+    double tempo_decorrido = 0.0;
+
+    double intervalo_medio_chegada = 0.01;
+    double largura_link;
+    double porc_ocupacao;
+
+    double chegada;
+    double servico;
+
+    double soma_tempo_servico = 0.0;
+
+    unsigned long int fila = 0;
+    unsigned long int max_fila = 0;
+
+    double e_n_final = 0.0;
+    double e_w_final = 0.0;
+    double lambda;
+
+    /**
+    Little
+    */
+    little e_n;
+    little e_w_chegada;
+    little e_w_saida;
+
+    inicia_little(&e_n);
+    inicia_little(&e_w_chegada);
+    inicia_little(&e_w_saida);
+    /**
+    Little -- fim
+    */
+
+    // srand(time(NULL));
+    srand(10000);
+
+    printf("Informe o percentual de ocupação desejado (entre 0 e 1): ");
+    scanf("%lF", &porc_ocupacao);
+    largura_link = (1 / intervalo_medio_chegada) * (0.1 * 1500 + 0.4 * 40 + 0.5 * 550) / porc_ocupacao;
+    printf("Largura do link: %lF", largura_link);
+    printf("\n%.2lF%%,0", porc_ocupacao * 100);
+
+    // gerando o tempo de chegada da primeira requisicao.
+    chegada = (-1.0 / (1.0 / intervalo_medio_chegada)) * log(aleatorio());
+    
+    double coleta_dados = 100.00;
+    while (tempo_decorrido <= tempo_simulacao)
+    {
+        double eventos[3] = {chegada, servico, coleta_dados};
+        tempo_decorrido = !fila ? minimo(chegada, coleta_dados) : minimo_a(eventos, 3);
+
+        // coleta de dados
+        if (tempo_decorrido == coleta_dados)
+        {
+
+            // printf("%lF,", coleta_dados * 100);
+            e_n.soma_areas += (tempo_decorrido - e_n.tempo_anterior) * e_n.no_eventos;
+            e_w_chegada.soma_areas += (tempo_decorrido - e_w_chegada.tempo_anterior) * e_w_chegada.no_eventos;
+            e_w_saida.soma_areas += (tempo_decorrido - e_w_saida.tempo_anterior) * e_w_saida.no_eventos;
+
+            e_w_saida.tempo_anterior = tempo_decorrido;
+            e_n.tempo_anterior = tempo_decorrido;
+            e_w_chegada.tempo_anterior = tempo_decorrido;
+
+            e_n_final = e_n.soma_areas / tempo_decorrido;
+            e_w_final = (e_w_chegada.soma_areas - e_w_saida.soma_areas) / (double)e_w_chegada.no_eventos;
+            lambda = e_w_chegada.no_eventos / tempo_decorrido;
+
+            E_N(printf(",%lF", e_n_final););
+            E_W(printf(",%lF", e_w_final););
+            ERRO_LITTLE(printf(",%.20lF", fabs(e_n_final - lambda * e_w_final)););
+            OCUPACAO(printf(",%lF", soma_tempo_servico / maximo(coleta_dados, servico)););
+            coleta_dados += 100.00;
+        }
+        // chegada
+        else if (tempo_decorrido == chegada)
+        {
+            // printf("Chegada em %lF.\n", tempo_decorrido);
+            if (!fila)
+            {
+                servico = tempo_decorrido + gera_pacote() / largura_link;
+                soma_tempo_servico += servico - tempo_decorrido;
+            }
+            fila++;
+            max_fila = fila > max_fila ? fila : max_fila;
+
+            chegada = tempo_decorrido + (-1.0 / (1.0 / intervalo_medio_chegada)) * log(aleatorio());
+
+            // little
+            e_n.soma_areas +=
+                (tempo_decorrido - e_n.tempo_anterior) * e_n.no_eventos;
+            e_n.tempo_anterior = tempo_decorrido;
+            e_n.no_eventos++;
+
+            e_w_chegada.soma_areas +=
+                (tempo_decorrido - e_w_chegada.tempo_anterior) * e_w_chegada.no_eventos;
+            e_w_chegada.tempo_anterior = tempo_decorrido;
+            e_w_chegada.no_eventos++;
+        }
+
+        // saida
+        else
+        {
+            // printf("Saida em %lF.\n", tempo_decorrido);
+            fila--;
+
+            if (fila)
+            {
+                servico = tempo_decorrido + gera_pacote() / largura_link;
+                soma_tempo_servico += servico - tempo_decorrido;
+            }
+
+            // little
+            e_n.soma_areas +=
+                (tempo_decorrido - e_n.tempo_anterior) * e_n.no_eventos;
+            e_n.tempo_anterior = tempo_decorrido;
+            e_n.no_eventos--;
+
+            e_w_saida.soma_areas +=
+                (tempo_decorrido - e_w_saida.tempo_anterior) * e_w_saida.no_eventos;
+            e_w_saida.tempo_anterior = tempo_decorrido;
+            e_w_saida.no_eventos++;
+        }
+    }
+    e_w_chegada.soma_areas +=
+        (tempo_decorrido - e_w_chegada.tempo_anterior) * e_w_chegada.no_eventos;
+
+    e_w_saida.soma_areas +=
+        (tempo_decorrido - e_w_saida.tempo_anterior) * e_w_saida.no_eventos;
+
+    e_n_final = e_n.soma_areas / tempo_decorrido;
+    e_w_final =
+        (e_w_chegada.soma_areas - e_w_saida.soma_areas) / e_w_chegada.no_eventos;
+    lambda = e_w_chegada.no_eventos / tempo_decorrido;
+
+    VALORES_FINAIS(
+        puts("\nResultado final:");
+
+        printf("E[N]: %lF\n", e_n_final);
+        printf("E[W]: %lF\n", e_w_final);
+        printf("lambda: %lF\n\n", lambda);
+        printf("Erro de Little: %.20lF\n\n", fabs(e_n_final - lambda * e_w_final));
+        printf("Ocupacao: %lF.\n", soma_tempo_servico / maximo(tempo_decorrido, servico));
+        printf("Max fila: %ld.\n", max_fila);)
+
+    return 0;
+}
+
+// int main() {
+//     // Capacity of 10 elements
+//     MinHeap* heap = init_minheap(10);
+
+//     insert_minheap(heap, (Event) {1, 3.12321});
+//     insert_minheap(heap, (Event) {1, 10.00});
+//     insert_minheap(heap, (Event) {1, 4.0});
+//     insert_minheap(heap, (Event) {1, 3.12319});
+//     insert_minheap(heap, (Event) {1, 20.0});
+
+//     print_heap(heap);
+    
+//     // Delete the heap->arr[1] (50)
+//     delete_element(heap, 1);
+
+//     print_heap(heap);
+
+//     // Delete the minimum element
+//     delete_minimum(heap);
+//     print_heap(heap);
+//     free_minheap(heap);
+//     return 0;
+// }
