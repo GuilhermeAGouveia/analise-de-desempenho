1d7ef312e45d46ccf763969b5194a8d8ac56f2b4
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2023-02-04 18:11:15 -0300

    refactor: directory structure

--- trabalhos/trabalho2/simulacao_web/utils/generate_csv.sh
+++ trabalhos/trabalho2/simulacao_web/utils/generate_csv.sh
@@ -2,6 +2,7 @@ gcc simulacao_web.c -o sim -lm
 
 for i in 0.6 0.8 0.95 0.99; do 
     a=$1
+    mkdir ./csv/$1
     a=$a$i
-    echo $i | ./sim >> ./csv/$a.dat
+    echo $i | ./sim >> ./csv/$1/$a.dat
 done
